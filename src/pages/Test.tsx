adc47a5a2f81b20c8c42dd87ac3a1d892660ceab
Yehoonn <jes6208@naver.com>
2024-06-12 18:06:31 +0900

    asd

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 // import { useMutation, useQuery } from '@apollo/client';
 // import { GET, POST } from '../graphQL/graphQL';
