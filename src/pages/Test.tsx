be707cc7e7f39b0c3dc0340e9f395fb62c417e45
Yehoonn <jes6208@naver.com>
2024-06-13 15:55:01 +0900

    test

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -270,7 +270,7 @@ const Test = () => {
               display: 'flex',
               justifyContent: 'center',
               width: '100vw',
-              height: '90vh',
+              height: '60vh',
             }}
           >
             <Webcam
