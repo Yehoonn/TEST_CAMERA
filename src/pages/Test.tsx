4ca2857ae2a870f468190484e176310929959ad3
Yehoonn <jes6208@naver.com>
2024-06-13 10:08:57 +0900

    TEST

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,99 +1,127 @@
-import React, { useEffect, useRef } from 'react';
-
-// import { useMutation, useQuery } from '@apollo/client';
-// import { GET, POST } from '../graphQL/graphQL';
-
-// const [addHuman] = useMutation(POST.createHuman);
-// const { loading, error, data, refetch } = useQuery(GET.humans);
-
-// const [input, setInput] = useState({
-//   age: 1,
-//   name: '',
-// });
-
-// const postData = async () => {
-//   try {
-//     // 뮤테이션 실행
-//     const { data } = await addHuman({
-//       variables: { name: input.name, age: input.age },
-//     });
-//     console.log(data);
-//   } catch (error) {
-//     console.error('Error : ', error);
-//   }
-// };
-
-// const refresh = () => {
-//   refetch();
-// };
-
-// console.log(data);
-
-// return (
-//   <div>
-//     {/* <div onClick={setToken}>발급</div>
-// <div onClick={verifyToken}>검증</div> */}
-//     <input
-//       placeholder="age"
-//       type="number"
-//       value={input.age}
-//       onChange={(e) => {
-//         setInput({ ...input, age: Number(e.target.value) });
-//       }}
-//     ></input>
-//     <input
-//       placeholder="name"
-//       type="string"
-//       value={input.name}
-//       onChange={(e) => {
-//         setInput({ ...input, name: e.target.value });
-//       }}
-//     ></input>
-//     <div onClick={postData}>데이터 생성</div>
-//     <div onClick={refresh}>데이터 가져오기</div>
-//     <div>
-//       {data?.humans?.map((value: any, index: any) => {
-//         return <div key={index}>{value.age}</div>;
-//       })}
-//     </div>
-//   </div>
-// );
+import React, { useState, useRef, useEffect } from 'react';
+import imageCompression from 'browser-image-compression';
 
 const Test = () => {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [loading, setLoading] = useState(false);
 
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
   useEffect(() => {
-    async function getMedia() {
+    async function getCameraStream() {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({
           video: true,
         });
         if (videoRef.current) {
-          videoRef.current.srcObject = stream;
+          (videoRef.current as HTMLVideoElement).srcObject = stream;
         }
       } catch (err) {
-        console.error('Error accessing media devices.', err);
+        console.error('Error accessing camera: ', err);
       }
     }
-
-    getMedia();
+    getCameraStream();
   }, []);
+  const captureImage = () => {
+    if (canvasRef.current && videoRef.current) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
+      if (context != null) {
+        context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+        canvas.toBlob(async (blob) => {
+          if (blob) {
+            const file = new File([blob], 'capture.png', { type: 'image/png' });
+            setLoading(true);
+            const compressedFile = await compressImage(file);
 
+            console.log(compressedFile);
+          }
+        }, 'image/png');
+      }
+    }
+  };
+  const openMobileCam = () => {
+    captureImage();
+  };
+  const compressImage = async (file: File) => {
+    const options = {
+      maxSizeMB: 0.2,
+      maxWidthOrHeight: 1024,
+      useWebWorker: true,
+    };
+    try {
+      const compressedFile = await imageCompression(file, options);
+      return compressedFile;
+    } catch (error) {
+      console.error('Image compression error:', error);
+      return file;
+    }
+  };
+
+  const cameraStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '250px',
+    background: `
+      linear-gradient(to right, #21005D 4px, transparent 4px) 0 0,
+      linear-gradient(to right, #21005D 4px, transparent 4px) 0 100%,
+      linear-gradient(to left, #21005D 4px, transparent 4px) 100% 0,
+      linear-gradient(to left, #21005D 4px, transparent 4px) 100% 100%,
+      linear-gradient(to bottom, #21005D 4px, transparent 4px) 0 0,
+      linear-gradient(to bottom, #21005D 4px, transparent 4px) 100% 0,
+      linear-gradient(to top, #21005D 4px, transparent 4px) 0 100%,
+      linear-gradient(to top, #21005D 4px, transparent 4px) 100% 100%
+    `,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '20px 20px',
+  };
   return (
-    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
-      <h1>Camera Access</h1>
-      <div
-        style={{
-          width: '600px',
-          height: '400px',
-          border: '1px solid black',
-          margin: '0 auto',
-        }}
-      >
-        <video ref={videoRef} width="100%" height="100%" autoPlay></video>
-      </div>
-    </div>
+    <>
+      <header className="header">
+        <button
+          type="button"
+          className="btn_back"
+          onClick={() => {
+            console.log('테스트');
+          }}
+        >
+          뒤로가기
+        </button>
+      </header>
+      <main className="container type_btn" style={{ alignItems: 'baseline' }}>
+        <article className="page_licenses" style={{ width: '100%' }}>
+          <div>
+            <>
+              차량 예약을 위해
+              <br />
+              <span className="point">운전면허증을 준비해 주세요.</span>
+            </>
+          </div>
+          <div className="box_sample" style={{ alignItems: 'center' }}>
+            <div style={cameraStyle}>
+              <video
+                ref={videoRef}
+                autoPlay
+                style={{ width: '90%', alignItems: 'center' }}
+              ></video>
+            </div>
+            <canvas
+              ref={canvasRef}
+              style={{ display: 'none' }}
+              width="640"
+              height="480"
+            ></canvas>
+          </div>
+        </article>
+        <div>어두운 배경에 가로로 놓아주세요.</div>
+        <div>빛 반사를 없애 주세요.</div>
+        <div>카메라 초점을 맞추고 촬영해 주세요.</div>
+        <button onClick={openMobileCam} type="button" className="btn">
+          운전면허증 촬영하기
+        </button>
+      </main>
+    </>
   );
 };
-
 export default Test;
