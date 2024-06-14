49cc07ab2288799f25747d79eaf97ef7c48b0247
Yehoonn <jes6208@naver.com>
2024-06-14 14:56:14 +0900

    asdfafsdasdf

--- src/pages/TestAndroid.tsx
+++ src/pages/TestAndroid.tsx
@@ -147,7 +147,7 @@ const TestAndroid = () => {
               display: 'flex',
               justifyContent: 'center',
               width: '95vw',
-              height: '212px',
+              height: '235px',
             }}
           >
             {selectedDeviceId !== '' && (
